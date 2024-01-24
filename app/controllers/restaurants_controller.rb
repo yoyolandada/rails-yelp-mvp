bb776e87fccfd05d4007e9d7eb69ec041e1f6dc1
yolanda martinez priego <yulielx@gmail.com>
2024-01-24 15:43:06 +0000

    Create restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,26 @@
 class RestaurantsController < ApplicationController
+  # before_action :set_restaurant, only: %i[show edit update destroy ]
+
   def index
     @restaurants = Restaurant.all
-    # raise
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create!(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
